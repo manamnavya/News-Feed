6ed901cec49588da5ae9726f71241859cdf34348
NavyaManam <manamnavya104@gmail.com>
2018-11-14 10:19:52 -0500

    Initial commit

---
+++ app/src/main/java/com/example/srivi/newsfeed/News.java
@@ -0,0 +1,30 @@
+/*
+    Homework 4
+    News.java
+    Navya Manam
+    Sri Vishnu Yandrapati
+ */
+package com.example.srivi.newsfeed;
+
+/**
+ * Created by srivi on 21-02-2018.
+ */
+
+public class News {
+    String title;
+    String publishedAt;
+    String imageUrl;
+    String description;
+    String link;
+
+    @Override
+    public String toString() {
+        return "News{" +
+                "title='" + title + '\'' +
+                ", publishedAt='" + publishedAt + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", description='" + description + '\'' +
+                ", link='" + link + '\'' +
+                '}';
+    }
+}
